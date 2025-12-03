642bc8456a68ddc426a9221ea5e1f99be14a697c
Leon Schuermann <leon@is.currently.online>
2025-12-03 14:51:23 -0500

    omniglot/build.rs: fix warning text for unsound benchmark features

--- omniglot/build.rs
+++ omniglot/build.rs
@@ -1,11 +1,11 @@
-fn unsound_feature_warn(feature: &str) {
+fn unsound_feature_warn(feature: &str, explanation: &str) {
     println!(
         "cargo:warning=\n\
 	 cargo:warning=--------------------------------------------------\n\
 	 cargo:warning=\n\
          cargo:warning=!!!DANGER!!!\n\
          cargo:warning=\n\
-         cargo:warning=OMNIGLOT FOREIGN MEMORY REFERENCE UPGRADE CHECKS ARE\n\
+         cargo:warning=OMNIGLOT {} ARE\n\
 	 cargo:warning=DISABLED. THIS IS UNSOUND.\n\
          cargo:warning=\n\
          cargo:warning=The feature\n\
@@ -17,16 +17,22 @@ fn unsound_feature_warn(feature: &str) {
 	 cargo:warning=--------------------------------------------------\n\
          cargo:warning=\n\
 	 ",
-        feature,
+        explanation, feature,
     );
 }
 
 fn main() {
     if cfg!(feature = "disable_upgrade_checks") {
-        unsound_feature_warn("disable_upgrade_checks");
+        unsound_feature_warn(
+            "disable_upgrade_checks",
+            "FOREIGN MEMORY REFERENCE UPGRADE CHECKS",
+        );
     }
 
     if cfg!(feature = "disable_validation_checks") {
-        unsound_feature_warn("disable_validation_checks");
+        unsound_feature_warn(
+            "disable_validation_checks",
+            "FOREIGN MEMORY REFERENCE VALIDATION CHECKS",
+        );
     }
 }
